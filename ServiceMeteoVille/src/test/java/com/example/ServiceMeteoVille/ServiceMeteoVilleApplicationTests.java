e8109ba662168f37d3b572a3b30d0ea07869abae
Manel-D <72567191+Manel-D@users.noreply.github.com>
2021-11-25 09:28:04 +0100

    creation de service meteo ville

---
+++ ServiceMeteoVille/src/test/java/com/example/ServiceMeteoVille/ServiceMeteoVilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiceMeteoVille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMeteoVilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
